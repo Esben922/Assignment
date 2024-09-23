fbf3bd5254d2ae0d5578218f034df91e6a68553e
Esben922 <TR67NH@student.aau.dk>
2024-09-23 09:45:08 +0200

    From Region data to Kiva Loans Data

--- app.py
+++ app.py
@@ -8,10 +8,11 @@ from scipy.stats import zscore
 import streamlit as st
 import altair as alt
 
-df = pd.read_csv('loan_themes_by_region.csv')
+df = pd.read_csv('kiva_loans.csv')
+df1 = pd.read_csv('loan_themes_by_region.csv')
+df2 = pd.read_csv('loan_theme_ids.csv')
 #Cleaning from last assginment
-df.drop(['Partner ID','Loan Theme ID','forkiva','geocode_old','ISO', 'geocode','names','geo','mpi_region','mpi_geo','rural_pct'], axis="columns", inplace=True)
-z_scores = zscore(df['amount'])
+z_scores = zscore(df['loan_amount'])
 df['outlier_amount'] = (z_scores > 3) | (z_scores < -3)
 df = df[~df['outlier_amount']]
 
@@ -22,7 +23,12 @@ selected_country = st.sidebar.selectbox("Select Country", df['country'].unique()
 if not selected_country:
     st.warning("Please select a Country from the sidebar ⚠️")
     st.stop()
-filtered_df = df[df['country'] == (selected_country)]
+filtered_df = df[df['country'] == selected_country]
+filtered_df = filtered_df.merge(df2[['id', 'Loan Theme ID', 'Loan Theme Type', 'Partner ID']], how='left', left_on='id', right_on='id')
+df1_unique_partners = df1[['Partner ID', 'Field Partner Name']].drop_duplicates(subset=['Partner ID'])
+filtered_df = filtered_df.merge(df1_unique_partners, how='left', left_on='Partner ID', right_on='Partner ID')
+
+
 
 #Siderbar filter: Region
 loan_theme_types = filtered_df['Loan Theme Type'].unique().tolist()
@@ -39,7 +45,7 @@ st.markdown('Country: '+selected_country)
 
 st.header('Field Partner Amounts')
 chart = alt.Chart(filtered_df).mark_bar().encode(
-    y='sum(amount)',
+    y='sum(loan_amount)',
     x=alt.X('Field Partner Name', sort='-y', axis=alt.Axis(labelAngle=-45, labelOverlap=False, labelFontSize=10))
 ).properties(
     title='Field Partner Sum Amount'
@@ -50,10 +56,10 @@ st.altair_chart(chart, use_container_width=True)
 st.header('Field Partner Loan Theme Type Distribution')
 field_partner_options = filtered_df['Field Partner Name'].unique()  
 selected_partner = st.selectbox('Select a Field Partner', field_partner_options)  
-pie_chart_data = filtered_df[filtered_df['Field Partner Name'] == selected_partner].groupby('Loan Theme Type')['amount'].sum().reset_index()
-pie_chart_data.columns = ['Loan Theme Type', 'amount']
+pie_chart_data = filtered_df[filtered_df['Field Partner Name'] == selected_partner].groupby('Loan Theme Type')['loan_amount'].sum().reset_index()
+pie_chart_data.columns = ['Loan Theme Type', 'loan_amount']
 chart = alt.Chart(pie_chart_data).mark_arc().encode(
-    theta=alt.Theta(field='amount', type='quantitative'),  
+    theta=alt.Theta(field='loan_amount', type='quantitative'),  
     color=alt.Color(field='Loan Theme Type', type='nominal')  
 ).properties(
     title=f'Loan Amount Distribution by Sector for {selected_partner}',  
@@ -63,7 +69,7 @@ chart = alt.Chart(pie_chart_data).mark_arc().encode(
 st.altair_chart(chart, use_container_width=True)
 
 st.header('Data Exploration')
-st.caption('MEAN:'+ str(filtered_df['amount'].mean()))
-st.caption('MEDIAN:'+ str(filtered_df['amount'].median()))
-st.caption('MODE:'+ str(filtered_df['amount'].mode()))
+st.caption('MEAN:'+ str(filtered_df['loan_amount'].mean()))
+st.caption('MEDIAN:'+ str(filtered_df['loan_amount'].median()))
+st.caption('MODE:'+ str(filtered_df['loan_amount'].mode()))
 st.dataframe(filtered_df)
